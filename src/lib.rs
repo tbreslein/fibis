eae78ee9582db3976c65931584c7a83ad23ad1e6
Tommy Breslein <tommy.breslein@protonmail.com>
2024-12-23 13:54:31 +0100

    basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,42 @@
+use std::marker::PhantomData;
+
+use num_integer::Integer;
+
 #[derive(Debug, Clone)]
-pub struct BitSet<T, const Size: usize, const Min: usize, const Max: usize> {
-    data: [T; Size],
+pub struct BitSet<T: Integer + Copy, const N: usize, const MIN: usize, const MAX: usize> {
+    data: [u64; N],
     len: usize,
+    phantom: PhantomData<T>
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+impl<T: Integer + Copy, const N: usize, const LOWER: usize, const UPPER: usize> Default for BitSet<T, N, LOWER, UPPER> {
+    fn default() -> Self {
+        Self {
+            data: [0_u64; N],
+            len: 0,
+            phantom: PhantomData
+        }
+    }
+}
+
+impl<T: Integer + Copy, const N: usize, const LOWER: usize, const UPPER: usize> BitSet<T, N, LOWER, UPPER> {
+    const _N_FITS: () = assert!(N == ((UPPER - LOWER).div_ceil(64)));
+
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn capacity(&self) -> usize {
+        N
+    }
 }
 
 #[cfg(test)]
@@ -13,8 +44,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn construction() {
+        let x = BitSet::<usize, 1, 0, 4>::new();
+        assert_eq!(x.len(), 0);
     }
 }
